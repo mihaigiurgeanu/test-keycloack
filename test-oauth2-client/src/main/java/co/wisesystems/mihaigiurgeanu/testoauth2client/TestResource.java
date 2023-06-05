118c6a0e02fa45f264e02c0560453fc509b01917
Mihai Giurgeanu <mihaigiurgeanu@gmail.com>
2023-06-06 00:46:25 +0300

    connect client and server without access token

--- test-oauth2-client/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2client/TestResource.java
+++ test-oauth2-client/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2client/TestResource.java
@@ -1,20 +1,27 @@
 package co.wisesystems.mihaigiurgeanu.testoauth2client;
 
-import java.util.UUID;
-
 import jakarta.websocket.server.PathParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/resources/{resourceName}")
 public class TestResource {
     @GET
     @Produces("application/json")
     public TestData getResource(@PathParam("resuourceName") String resourceName) {
-    	TestData data = new TestData();
-    	data.setId(UUID.randomUUID().toString());
-    	data.setName(resourceName);
-        return data;
+
+    	Client client = ClientBuilder.newClient();
+    	TestData data = client
+    			.target("http://resource-server:8080/testoauth2server/v1")
+    			.path("/resouirces/testResource")
+    			.request()
+    			.accept(MediaType.APPLICATION_JSON)
+    			.get(TestData.class);
+    	
+    	return data;
     }
 }
