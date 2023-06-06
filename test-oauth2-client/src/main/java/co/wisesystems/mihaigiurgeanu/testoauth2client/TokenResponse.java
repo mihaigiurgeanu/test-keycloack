823f82d4fb10039c44bd459e0b3396ba2fb29ef9
Mihai Giurgeanu <mihaigiurgeanu@gmail.com>
2023-06-06 14:45:41 +0300

    add all fields for tocken response

--- test-oauth2-client/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2client/TokenResponse.java
+++ test-oauth2-client/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2client/TokenResponse.java
@@ -7,6 +7,11 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 public class TokenResponse {
 	
 	private String token;
+	private int expiresIn;
+	private int refreshExpiresIn;
+	private String tokenType;
+	private int notBeforePolicy;
+	private String scope;
 
 	public String getToken() {
 		return token;
@@ -15,6 +20,51 @@ public class TokenResponse {
 	@XmlElement(name = "access_token")
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	public int getExpiresIn() {
+		return expiresIn;
+	}
+
+	@XmlElement(name = "expires_in")
+	public void setExpiresIn(int expiresIn) {
+		this.expiresIn = expiresIn;
+	}
+
+	public int getRefreshExpiresIn() {
+		return refreshExpiresIn;
+	}
+
+	@XmlElement(name = "refresh_expires_in")
+	public void setRefreshExpiresIn(int refreshExpiresIn) {
+		this.refreshExpiresIn = refreshExpiresIn;
+	}
+
+	public String getTokenType() {
+		return tokenType;
+	}
+
+	@XmlElement(name = "token_type")
+	public void setTokenType(String tokenType) {
+		this.tokenType = tokenType;
+	}
+
+	public int getNotBeforePolicy() {
+		return notBeforePolicy;
+	}
+
+	@XmlElement(name = "not-before-policy")
+	public void setNotBeforePolicy(int notBeforePolicy) {
+		this.notBeforePolicy = notBeforePolicy;
+	}
+
+	public String getScope() {
+		return scope;
+	}
+
+	@XmlElement(name = "scope")
+	public void setScope(String scope) {
+		this.scope = scope;
 	}
 	
 	
