582fe3862a3e30600fbe82ad8ac8fb9163607a03
Mihai Giurgeanu <mihaigiurgeanu@gmail.com>
2023-06-06 21:10:07 +0300

    fix minor issues

--- test-oauth2-client/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2client/TestResource.java
+++ test-oauth2-client/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2client/TestResource.java
@@ -2,29 +2,33 @@ package co.wisesystems.mihaigiurgeanu.testoauth2client;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import jakarta.websocket.server.PathParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/resources/{resourceName}")
+@Path("/resources")
 public class TestResource {
 	
 	@Autowired
 	ServiceClientAccessToken accessTokenProvider;
 	
     @GET
+    @Path("/{resourceName}")
     @Produces("application/json")
-    public TestData getResource(@PathParam("resuourceName") String resourceName) {
+    public TestData getResource(@PathParam("resourceName") String resourceName) {
     	String token = accessTokenProvider.getToken();
+    	
     	
     	Client client = ClientBuilder.newClient();
     	TestData data = client
-    			.target("http://resource-server:8080/testoauth2server/v1")
-    			.path("/resouirces/testResource")
+    			.target("http://resource-server:8080")
+    			.path("/testoauth2server/v1")
+    			.path("resources")
+    			.path(resourceName)
     			.request()
     			.accept(MediaType.APPLICATION_JSON)
     			.header("Authorization", "Bearer " + token)
--- test-oauth2-server/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2server/TestResource.java
+++ test-oauth2-server/src/main/java/co/wisesystems/mihaigiurgeanu/testoauth2server/TestResource.java
@@ -1,16 +1,17 @@
 package co.wisesystems.mihaigiurgeanu.testoauth2server;
 import java.util.UUID;
 
-import jakarta.websocket.server.PathParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
-@Path("/resources/{resourceName}")
+@Path("/resources")
 public class TestResource {
     @GET
+    @Path("/{resourceName}")
     @Produces("application/json")
-    public TestData getResource(@PathParam("resuourceName") String resourceName) {
+    public TestData getResource(@PathParam("resourceName") String resourceName) {
     	TestData data = new TestData();
     	data.setId(UUID.randomUUID().toString());
     	data.setName(resourceName);
